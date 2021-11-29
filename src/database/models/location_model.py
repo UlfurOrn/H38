cd1ed5d557d81dedd76149a67de51ccd8fefb487
Ulfur_Orn <ulfurinn@gmail.com>
2021-11-29 13:42:22 +0000

    Location model finished

---
+++ src/database/models/location_model.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Optional
+from uuid import UUID
+
+from database.models.database_model import DatabaseModel
+
+
+class Location(DatabaseModel):
+    _HEADERS = ["id", "country", "airport", "phone", "opening_hours", "supervisor_id"]
+    _FILENAME = "locations.csv"
+
+    country: str
+    airport: str
+    phone: int
+    opening_hours: str
+    supervisor_id: Optional[UUID] = None
+
+    @classmethod
+    def serialize(cls, model: Location) -> dict:
+        return model.dict()
+
+    @classmethod
+    def deserialize(cls, data: dict) -> Location:
+        return Location(**data)
+
+
+if __name__ == "__main__":
+    DatabaseModel._PATH = "../data/"
+
+    Location(
+        country="Iceland",
+        airport="Keflavík, Airport",
+        phone=1234567,
+        opening_hours="08:00-20:00",
+        supervisor_id=UUID("c114a7b3-d5c9-490f-b82d-38709fe825f1"),
+    ).create()
+
+    print(Location.read())
