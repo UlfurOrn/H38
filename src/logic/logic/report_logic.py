11ef435401c115dc2b0337b86dea9950b950e908
Rikki <rikki.aron@gmail.com>
2021-11-30 13:33:49 +0000

    Added report logic

---
+++ src/logic/logic/report_logic.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from database.models.report_model import Report
+from database.models.
+from Verklegt_1.H38.src.logic.helpers import ListItem, Paginator
