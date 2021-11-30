b66daedbe79dcf0fcc44b001fc9893e1816c74c4
gudmundurbk <94848249+gudmundurbk@users.noreply.github.com>
2021-11-30 09:55:30 +0000

    Create models (#7)
    * added model for property and facility
    
    * added models for contractor, report and request
    
    * imported os for database_model
    
    * Apply suggestions from code review
    
    Co-authored-by: UlfurOrn <48811972+UlfurOrn@users.noreply.github.com>
    
    Co-authored-by: UlfurOrn <48811972+UlfurOrn@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+import sys
+import os
+from uuid import UUID
 from database.models.employee_model import Employee
+from database.models.property_model import Property
 
-if __name__ == "__main__":
-    employees = Employee.all()
+print("Here:", os.path.relpath(__file__))
 
-    employee = employees[0]
 
-    print(employee)
+employee = Employee.get(UUID("e42cee48-1424-45a3-95a6-3d6037a57d5f"))
+
+
+property = Property(
+    property_number = "property1234",
+    condition = "good",
+    location_id = "cd314c5c-1cc3-4376-9003-6529b14cda8f"
+).create()
