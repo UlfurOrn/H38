56ee64780b678208be98a638460a781e5f1fd382
Gnaeus-Pompeius <smarib20@ru.is>
2021-12-03 11:27:50 +0000

    fra smara view_window code

--- src/interface/windows/view_window.py
+++ src/interface/windows/view_window.py
@@ -6,6 +6,7 @@ from interface.windows.window import Button, Window
 from logic.api import api
 from logic.helpers import InfoModel
 from logic.logic.employee_logic import EmployeeInfo
+from logic.logic.location_logic import LocationInfo
 
 
 class Field(BaseModel):
@@ -56,4 +57,32 @@ class EmployeeViewWindow(ViewWindow):
     buttons = [
         Button(letter="u", description="update", function=update),
         Button(letter="b", description="back", function=None),
+    ]
+
+
+
+class LocationsViewWindow(ViewWindow):
+    title = "View Employee"
+    info: LocationInfo
+    fields = [
+        Field(name="Location_ID", field="location_id"),
+        Field(name="Country", field="country"),
+        Field(name="Airport", field="airport"),
+        Field(name="Supervisor_ID", field="supervisor_id"),
+        Field(name="Supervisor", field="supervisor"),
+    ]
+
+    def window_setup(self) -> None:
+        self.info = api.locations.get(self.model_id)
+
+    def update(self) -> None:
+        print("Update")
+
+    buttons = [
+        Button(letter="a", description="prev", function=update),
+        Button(letter="b", description="back", function=None),
+        Button(letter="c", description="create", function=None),
+        Button(letter="d", description="next", function=None),
+        Button(letter="f", description="filter", function=None),
+        Button(letter="s", description="sort", function=None),
     ]
