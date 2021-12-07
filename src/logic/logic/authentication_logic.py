b0a156849edc702fe7f89187e84fd5f7d0b92d89
Ulfur_Orn <ulfurinn@gmail.com>
2021-12-07 11:23:51 +0000

    Refactored auth manager a bit

--- src/logic/logic/authentication_logic.py
+++ src/logic/logic/authentication_logic.py
@@ -1,27 +1,19 @@
-from database.models.employee_model import Employee
+from src.database.models.employee_model import Employee
 from utils.authentication import AuthManager
 
 
-class Verification:
+class AuthenticationLogic:
     @staticmethod
     def login(ssn: int) -> None:
-        """Check if a user exists with this SSN. If not, raise an error otherwise
-        use AuthManger to set logged_in_user to the user found
-        """
         employees = Employee.all()
         for employee in employees:
             if employee.ssn == ssn:
-                AuthManager.logged_in_user = employee.id
+                AuthManager.set_user(employee)
                 return
 
         raise Exception("User not found in database")
 
     @staticmethod
-    def check_supervisor():
-        """Should fetch user with ID AuthManager.logged_in_user_id and raise exception
-        if not a supervisor
-        """
-        user_id = AuthManager.logged_in_user
-        user = Employee.get(user_id)
-        if not user.is_supervisor:
-            raise Exception("User is not a supervisor")
+    def is_supervisor():
+        user = AuthManager.get_user()
+        return user.is_supervisor()
